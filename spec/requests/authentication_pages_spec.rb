999232f787711b533c75f5814eb05aa69d35cf9d
jtmurphree <jtmurphree@gmail.com>
2013-12-09 18:44:54 -0600

    save canges

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -1,11 +1,10 @@
 require 'spec_helper'
+
 describe "Authentication" do
 	subject { page }
 	describe "signin page" do
 		before { visit signin_path }
-
-		it { should have_content('Sign in') }
-		it { should have_title('Sign in') }
+		it { should be_signin_page}
 	end
 
 	describe "signin" do
@@ -15,11 +14,11 @@ describe "Authentication" do
 			before { click_button "Sign in" }
 
 	 		it { should have_title('Sign in') }
-	 		it { should have_selector('div.alert.alert-error', text: 'Invalid') }
+	 		it { should have_error_message('Invalid') }
 
 	 		describe "after visiting another page" do
 	 			before { click_link "Home" }
-	 			it { should_not have_selector('div.alert.alert-error')} 
+	 			it { should_not have_error_message('Invalid')} 
 	 		end
 	 	end
 
@@ -31,14 +30,11 @@ describe "Authentication" do
 	 			click_button "Sign in"
 	 		end
 
-	 		it { should have_title(user.name) }
-	 		it { should have_link('Profile',		href: user_path(user)) }
-	 		it { should have_link('Sign out',		href: signout_path) }
-	 		it { should_not have_link('Sign in', 		href: signin_path)	}
+	 		it { should be_signedin(user) }
 
 	 		describe "followed by signout" do
 	 			before {click_link "Sign out" }
-	 			it { should have_link('Sign in')}
+	 			it { should be_signedout(user) }
 	 		end
 	 	end
 	end
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,10 +1,35 @@
 include ApplicationHelper
 
-#def full_title(page_title)
-#	base_title = "Ruby on Rails Tutorial Sample App"
-#	if page_title.empty?
-#		base_title
-#	else
-#		"#{base_title} | #{page_title}"
-#	end
-#end
+def valid_signin(user)
+	fill_in "Email",	with: user.Email
+	fill_in "Password",	wit: user.password
+	click_button "Sign in"
+end
+
+RSpec::Matchers.define :have_error_message do |message|
+	match do |page|
+		expect(page).to have_selector('div.alert-error', text: message)
+	end
+end
+
+RSpec::Matchers.define :be_signedin do |user|
+	match do |page|
+		page { should have_title(user.name)}
+		page { should have_link('Profile',		href: user_path(user)) }
+		page { should have_link('Sign out', 		href: signout_path) }
+		page { should_not have_link('Sin in', href: signin_path) }
+	end
+end
+
+RSpec::Matchers.define :be_signedout do |user|
+	match do |page|
+		page { should have_link('Sign in', href: signin_path) }
+	end
+end
+
+RSpec::Matchers.define :be_signin_page do
+	match do |page|
+		page { should have_content('Sign in') }
+		page { should have_title('Sign in') }
+	end
+end
